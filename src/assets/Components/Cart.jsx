db9f929fb31e1a5c57d097b6d8fd338df79babb1
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-11 20:08:21 +0100

    Cart Component added

--- src/assets/Components/Cart.jsx
+++ src/assets/Components/Cart.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types"
-import useHover from 
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import useHover from "../hooks/useHover";
 
+function CartItem({ item }) {
+  const [hovered, ref] = useHover();
+  const { removeFromCart } = useContext(Context);
 
-function CartItem ({item}) {
-    const []
+  const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
 
+  return (
+    <div className="cart-item">
+      <i
+        className="iconClassName"
+        onClick={() => removeFromCart(item.id)}
+        ref={ref}
+      ></i>
+
+      <img src={item.url} width="130px" />
+      <p>$5.99</p>
+    </div>
+  );
+
+  CartItem.PropTypes = {
+    item: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+    }),
+  };
 }
+
+export default CartItem;
--- src/assets/hooks/useHover.jsx
+++ src/assets/hooks/useHover.jsx
@@ -1,25 +1,28 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 
-function useHover () {
-    const [hovered, setHovered] = useState(false)
-    const ref = useRef(null)
+function useHover() {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
 
-    function enter() {
-        setHovered(true)
-    }
+  function enter() {
+    setHovered(true);
+  }
 
-    function leave() {
-        setHovered(false)
-    }
+  function leave() {
+    setHovered(false);
+  }
 
-    useEffect(function () {
-        ref.current.addEventListener("mouseeneter", enter )
-        ref.current.addEventListener("mouseleave", leave)
+  useEffect(function () {
+    ref.current.addEventListener("mouseeneter", enter);
+    ref.current.addEventListener("mouseleave", leave);
 
-        return () => {
-            ref.current.remove
-        }
+    return () => {
+      ref.current.removeEventListener("mouseenter", enter);
+      ref.current.removeEventListener("mouseleave", leave);
+    };
+  }, []);
 
-    })
-
+  return [hovered, ref];
 }
+
+export default useHover;
